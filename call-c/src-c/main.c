b66a2a6f9c324cf385712f26d76ab10e01878d64
dot96gal <dot96gal@gmail.com>
2025-03-12 19:13:34 +0900

    add call-c

---
+++ call-c/src-c/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "calc.h"
+
+int main(void)
+{
+  int x = 5;
+  int y = 16;
+
+  int z = add(x, y);
+
+  printf("%d + %d = %d\n", x, y, z);
+
+  return 0;
+}
+
---
+++ call-c/src-zig/main.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+const calc = @cImport({
+    @cInclude("calc.c");
+});
+
+fn add(x: i32, y: i32) i32 {
+    return calc.add(x, y);
+}
+
+pub fn main() !void {
+    const x: i32 = 5;
+    const y: i32 = 16;
+    const z: i32 = add(x, y);
+
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("{d} + {d} = {d}\n", .{ x, y, z });
+    try bw.flush();
+}
+
+test "test add" {
+    try std.testing.expectEqual(@as(i32, 21), add(5, 16));
+}
